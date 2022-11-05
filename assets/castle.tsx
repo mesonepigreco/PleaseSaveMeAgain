4c0abaf1e949c2b9c0dfa1f322c77851dfe0808d
Lorenzo Monacelli <mesonepigreco@gmail.com>
2022-11-05 15:42:19 +0100

    Added some tilesets to try to get something

---
+++ assets/castle.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="castle" tilewidth="8" tileheight="8" tilecount="10" columns="10">
+ <image source="castle_floor.png" width="80" height="8"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="2" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="2"/>
+   <object id="2" class="collision" x="0" y="0" width="2" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="6" y="0" width="2" height="8"/>
+   <object id="2" class="collision" x="0" y="0" width="8" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="6" width="8" height="2"/>
+   <object id="2" class="collision" x="6" y="0" width="2" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="6" width="8" height="2"/>
+   <object id="2" class="collision" x="0" y="0" width="2" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="6" width="8" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="6" y="0" width="2" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/castle_2.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="castle_2" tilewidth="8" tileheight="8" tilecount="14" columns="14">
+ <image source="castle_floor.png" width="112" height="8"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="5" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="7"/>
+   <object id="2" class="collision" x="0" y="0" width="5" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="7"/>
+   <object id="2" class="collision" x="3" y="0" width="5" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="3" y="0" width="5" height="8"/>
+   <object id="2" class="collision" x="0" y="1" width="8" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="1" width="8" height="7"/>
+   <object id="2" class="collision" x="0" y="0" width="5" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="1" width="8" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="3" y="0" width="5" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="0" width="5" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="3" y="0" width="5" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="3" y="1" width="5" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" class="collision" x="0" y="1" width="5" height="7"/>
+  </objectgroup>
+ </tile>
+</tileset>
